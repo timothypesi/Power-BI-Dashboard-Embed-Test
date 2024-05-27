566a7446676667f39a75b0aeb0bb3059f3d67caa
Timothy pesi <48859349+timothypesi@users.noreply.github.com>
2024-05-27 21:33:26 +0300

    Add files via upload

--- embed.py
+++ embed.py
@@ -21,7 +21,7 @@ def main():
             
             # Embed the Power BI dashboard
             power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiZDVmYTdmYjctOWYxMy00YjRlLTk4OTUtOTQyYzc5YzA5ZWI5IiwidCI6ImI0YTc0NjQyLWFkOTMtNGQ1Ni1iOWVjLWU2YzRmZTA4MGE3ZSIsImMiOjN9"
-            components.html(f'<iframe src="{power_bi_url}" width="800" height="600"></iframe>', height=600)
+            components.html(f'<iframe src="{power_bi_url}" width="600" height="375"></iframe>', height=400)
         else:
             st.error("Incorrect password. Please try again.")
 
